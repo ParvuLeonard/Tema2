8777014ed46dfd8a97e5ee8582b699203a85500d
Parvu Leonard <parvu.leonard@yahoo.com>
2015-04-27 02:21:01 +0300

    First Commit

---
+++ StoreMVC/StoreMVC/StoreMVC/Controllers/ListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoreMVC.Models;
+
+namespace StoreMVC.Controllers
+{
+    public class ListController : Controller
+    {
+        private ProductDBContext de = new ProductDBContext();
+        public ActionResult Index()
+        {
+            ViewBag.listProducts = de.Products.ToList();
+            return View();
+        }
+
+    }
+}
---
+++ StoreMVC/StoreMVC/StoreMVC/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreMVC.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class ProductDBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
